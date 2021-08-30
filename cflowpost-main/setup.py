53da70d3d39e9d221ccfc098e2e78d3e253b252d
Ricardo Franco <ricardofranco95e@gmail.com>
2021-08-30 12:14:44 -0500

    added requirements

--- cflowpost-main/setup.py
+++ cflowpost-main/setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+requirements = ["numpy", "pandas", "matplotlib",
+                "scipy", "seaborn", "toolz"]
+
 setup(name="cflowpost",
       author="Ricardo Franco Estrada",
       author_email="ricardo.franco@pucp.edu.pe",
       packages=find_packages(where="src", include=["cflowpost.*"]),
-      package_dir={"": "src"})
+      package_dir={"": "src"},
+      install_requires=requirements)
