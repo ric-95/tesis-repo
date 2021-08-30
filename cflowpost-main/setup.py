8ec4d9d1c34c88c6d8febe3495499ae7705f1382
Ricardo Franco <ricardofranco95e@gmail.com>
2021-08-30 15:52:55 -0500

    added version number, install requirements and package data

--- cflowpost-main/setup.py
+++ cflowpost-main/setup.py
@@ -6,6 +6,8 @@ requirements = ["numpy", "pandas", "matplotlib",
 setup(name="cflowpost",
       author="Ricardo Franco Estrada",
       author_email="ricardo.franco@pucp.edu.pe",
+      version="0.1.0",
       packages=find_packages(where="src", include=["cflowpost.*"]),
       package_dir={"": "src"},
-      install_requires=requirements)
+      install_requires=requirements,
+      package_data={"cflowpost.pvx": ["README.md"]})
